5980c57148f79e93866bb306365fda994fe96f72
Lars Yencken <lars@yencken.org>
2014-12-31 15:55:34 +1100

    Make the public API small but complete.

--- csvdiff/__init__.py
+++ csvdiff/__init__.py
@@ -26,7 +26,7 @@ def diff_files(from_file, to_file, index_columns):
     """
     from_records = records.load(from_file)
     to_records = records.load(to_file)
-    return records.compare(from_records, to_records, index_columns)
+    return patch.create(from_records, to_records, index_columns)
 
 
 def diff_records(from_records, to_records, index_columns):
@@ -34,40 +34,43 @@ def diff_records(from_records, to_records, index_columns):
     Diff two sequences of dictionary records, returning the patch which
     transforms one into the other.
     """
-    return records.compare(from_records, to_records, index_columns)
+    return patch.create(from_records, to_records, index_columns)
 
 
-def diff_and_summarize(from_csv, to_csv, index_columns, stream=sys.stdout):
+def patch_file(patch_stream, from_file, to_file, strict=True):
     """
-    Print a summary of the difference between the two files.
+    Apply the patch to the source CSV file, and save the result to the target
+    file.
     """
-    from_records = records.load(from_csv)
-    to_records = records.load(to_csv)
-    diff = records.compare(from_records, to_records, index_columns)
-    summarize_diff(diff, len(from_records), stream=stream)
+    diff = patch.load(patch_file)
+    from_records = records.load(from_file)
+    to_records = patch.apply(diff, from_records, strict=strict)
 
-
-def summarize_diff(diff, orig_size, stream=sys.stdout):
-    if orig_size == 0:
-        # slightly arbitrary when the original data was empty
-        orig_size = 1
-
-    n_removed = len(diff['removed'])
-    n_added = len(diff['added'])
-    n_changed = len(diff['changed'])
-
-    if n_removed or n_added or n_changed:
-        print(u'%d rows removed (%.01f%%)' % (
-            n_removed, 100 * n_removed / orig_size
-        ), file=stream)
-        print(u'%d rows added (%.01f%%)' % (
-            n_added, 100 * n_added / orig_size
-        ), file=stream)
-        print(u'%d rows changed (%.01f%%)' % (
-            n_changed, 100 * n_changed / orig_size
-        ), file=stream)
+    # what order should the columns be in?
+    if to_records:
+        # have data, use a nice ordering
+        all_columns = to_records[0].keys()
+        index_columns = diff['_index']
+        fieldnames = _nice_fieldnames(all_columns, index_columns)
     else:
-        print('files are identical')
+        # no data, use the original order
+        fieldnames = from_records.fieldnames
+
+    records.save(to_records, fieldnames, to_file)
+
+
+def patch_records(diff, from_records, strict=True):
+    """
+    Apply the patch to the sequence of records, returning the transformed
+    records.
+    """
+    return patch.apply(diff, from_records, strict=strict)
+
+
+def _nice_fieldnames(all_columns, index_columns):
+    "Indexes on the left, other fields in alphabetical order on the right."
+    non_index_columns = set(all_columns).difference(index_columns)
+    return index_columns + sorted(non_index_columns)
 
 
 class CSVType(click.ParamType):
@@ -114,13 +117,52 @@ def csvdiff_cmd(index_columns, from_csv, to_csv, style=None, output=None):
                else open(output, 'w'))
 
     if style == 'summary':
-        diff_and_summarize(from_csv, to_csv, index_columns, ostream)
+        _diff_and_summarize(from_csv, to_csv, index_columns, ostream)
     else:
         compact = (style == 'compact')
-        diff = diff_files(from_csv, to_csv, index_columns)
-        patch.save(diff, ostream, compact=compact)
+        _diff_files_to_stream(from_csv, to_csv, index_columns, ostream,
+                              compact=compact)
 
     ostream.close()
+
+
+def _diff_files_to_stream(from_csv, to_csv, index_columns, ostream,
+                          compact=False):
+    diff = diff_files(from_csv, to_csv, index_columns)
+    patch.save(diff, ostream, compact=compact)
+
+
+def _diff_and_summarize(from_csv, to_csv, index_columns, stream=sys.stdout):
+    """
+    Print a summary of the difference between the two files.
+    """
+    from_records = records.load(from_csv)
+    to_records = records.load(to_csv)
+    diff = patch.create(from_records, to_records, index_columns)
+    _summarize_diff(diff, len(from_records), stream=stream)
+
+
+def _summarize_diff(diff, orig_size, stream=sys.stdout):
+    if orig_size == 0:
+        # slightly arbitrary when the original data was empty
+        orig_size = 1
+
+    n_removed = len(diff['removed'])
+    n_added = len(diff['added'])
+    n_changed = len(diff['changed'])
+
+    if n_removed or n_added or n_changed:
+        print(u'%d rows removed (%.01f%%)' % (
+            n_removed, 100 * n_removed / orig_size
+        ), file=stream)
+        print(u'%d rows added (%.01f%%)' % (
+            n_added, 100 * n_added / orig_size
+        ), file=stream)
+        print(u'%d rows changed (%.01f%%)' % (
+            n_changed, 100 * n_changed / orig_size
+        ), file=stream)
+    else:
+        print('files are identical')
 
 
 @click.command()
@@ -129,30 +171,23 @@ def csvdiff_cmd(index_columns, from_csv, to_csv, style=None, output=None):
               help='Read the JSON patch from the given file.')
 @click.option('--output', '-o', type=click.Path(),
               help='Write the transformed CSV to the given file.')
-def patch_cmd(input_file, input=None, output=None):
+@click.option('--strict/--no-strict', default=True,
+              help='Whether or not to tolerate a changed source document '
+                   '(default: strict)')
+def patch_cmd(input_file, input=None, output=None, strict=True):
     """
     Apply the changes from a csvdiff patch to an existing CSV file.
     """
-    istream = (sys.stdin
-               if input is None
-               else open(input))
-    ostream = (sys.stdout
-               if output is None
-               else open(output, 'w'))
+    patch_stream = (sys.stdin
+                    if input is None
+                    else open(input))
+    tocsv_stream = (sys.stdout
+                    if output is None
+                    else open(output, 'w'))
 
     try:
-        patch_stream(input_file, istream, ostream)
+        patch_file(patch_stream, input_file, tocsv_stream, strict=strict)
 
     finally:
         input.close()
         output.close()
-
-
-def patch_stream(orig_file, patch_stream, ostream):
-    diff = patch.load(patch_stream)
-    orig = records.load(orig_file)
-    patched = patch.apply(diff, orig)
-    fieldnames = (sorted(patched[0].keys())
-                  if patched
-                  else orig.fieldnames)
-    records.save(patched, fieldnames, ostream)
